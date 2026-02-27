dba20848f421a09554d475404763c94e3c0119d1
Narinde Singh Beela <nsb619236@gmail.com>
2026-02-27 17:03:02 +0530

    Update sw.js
    Signed-off-by: Narinde Singh Beela <nsb619236@gmail.com>

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('waves-cache').then(cache => {
+      return cache.addAll(['/']);
+    })
+  );
+});
 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
